3aae09df872df72cf65bd37d1d3a5711321927a7
Fede Sandoval <federico.sandoval@unq.edu.ar>
2023-09-04 18:46:00 -0300

    :sparkles: integracion de funcionalidades para la segunda entrega
    Signed-off-by: Fede Sandoval <federico.sandoval@unq.edu.ar>

--- modules/SunQLite.py
+++ modules/SunQLite.py
@@ -1,15 +1,43 @@
 from modules.compiler import Compiler
-from modules.vm import VirtualMachine
+from modules.table import Table
 
 
 class SunQLite:
-    def __init__(self, compiler: Compiler, vm: VirtualMachine):
-        self.compiler = compiler
-        self.vm = vm
-        self.can_exec = True
+    def __init__(self):
+        self.compiler = Compiler()
+        self.keep_running = True
+        self.table = Table("user")
 
     def start(self):
-        while self.can_exec:
-            user_input = input("sql>")  # Espera la entrada del usuario
-            command = self.compiler.do(user_input)
-            self.vm.do(command)
+        command_global = None
+
+        while self.keep_running:
+            user_input = input("sql>")
+            try:
+                command_global = self.compiler.do(user_input)
+            except Exception as e:
+                # TODO: pasar comillas dobles a simples x convencion Python
+                print("Se produjo un error en compilación")
+                raise e
+
+            try:
+                command_global.do(self)
+            except Exception as e:
+                print("Se produjo un error en ejecución")
+                raise e
+
+    def insert(self, record):
+        self.table.insert(record)
+        print("INSERT exitoso")
+
+    def select(self):
+        for record in self.table.select():
+            print(record[0], record[1], record[2])
+
+    def finish(self):
+        self.keep_running = False
+
+    def metadata(self):
+        metadata = self.table.metadata()
+        print('Paginas: ' + str(metadata[0]))
+        print('Registros: ' + str(metadata[1]))
